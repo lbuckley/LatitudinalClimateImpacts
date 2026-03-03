9ea526eeecc7de5190f97c1ef4048c0d59fd63a0
Lauren Buckley <l.b.buckley@gmail.com>
2026-03-03 15:25:43 -0800

    Update fig1

--- Figure1.R
+++ Figure1.R
@@ -2,6 +2,7 @@ library(GSODR)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(TrenchR)
 
 #FIG 1a, seasonal temperature variation
 #find stations
@@ -30,7 +31,7 @@ tmean <- tdat %>%
   mutate(period = ifelse(YEAR <= 2000, "1980_1984", "2020_2024")) %>%
   group_by(CTRY, period, YDAY) %>%
   summarise(
-    tmean_mean = mean(TEMP, na.rm = TRUE),
+    tmean_mean = mean((MIN+MAX)/2, na.rm = TRUE), #CHECK MEAN
     tmin_mean = mean(MIN, na.rm = TRUE),
     tmax_mean = mean(MAX, na.rm = TRUE),
     .groups   = "drop"
@@ -48,88 +49,48 @@ tmean_l<- tmean_l[order(tmean_l$YDAY),]
 #plot
 fig1a= ggplot(tmean_l, aes(x=YDAY, colour=CTRY, lty=period)) + 
   geom_line(aes(y=temp),alpha=0.5)+
+  #geom_smooth(aes(y=temp), method="loess", se=FALSE)+
   geom_smooth(aes(y=tmean_mean), method="loess", se=FALSE)+
   scale_color_manual(values=c("darkorange","cornflowerblue"))+
   theme_classic()+
   ylab("Temperature (C)")+
   xlab("Day of year")
 
-#time period issue?
+#mean annual temperatures
+tann <- tmean %>%
+  group_by(CTRY, period) %>%
+  summarise(
+    tmean_mean = mean(tmean_mean), 
+    tmin_mean = mean(tmin_mean),
+    tmax_mean = mean(tmax_mean),
+    tmean_sd= sd(tmean_mean),
+    .groups   = "drop"
+  )
+
+tann<- as.data.frame(tann)
+tann$loc<- ifelse(tann$CTRY=="MX", "trop", "temp")
 
 #-------------------------
 #FIG 1b, TPCs
 
-# Schematic TPCs (as before)
-tpc_schematic <- tibble::tribble(
-  ~temp, ~perf, ~climate,
-  0, 0.00, "Temperate",
-  5, 0.20, "Temperate",
-  10, 0.50, "Temperate",
-  15, 0.80, "Temperate",
-  20, 1.00, "Temperate",
-  25, 0.80, "Temperate",
-  30, 0.40, "Temperate",
-  35, 0.10, "Temperate",
-  15, 0.00, "Tropical",
-  20, 0.30, "Tropical",
-  25, 0.80, "Tropical",
-  28, 1.00, "Tropical",
-  31, 0.80, "Tropical",
-  34, 0.30, "Tropical",
-  37, 0.00, "Tropical"
-)
+temps=1:40
 
-# Schematic horizontal lines:
-# y_past_mean, y_future_mean, and a y-range for body temperatures
-horiz <- tibble::tribble(
-  ~climate,    ~y_past, ~y_future, ~y_body_low, ~y_body_high,
-  "Temperate",   0.25,     0.35,       0.45,        0.75,
-  "Tropical",    0.25,     0.35,       0.55,        0.85
-)
+temp.tpc<- TPC(temps, 18, 5, 28)*0.8 #scale to preserve area
+trop.tpc<- TPC(temps, 28, 20, 32)
 
-# Choose x positions for the horizontal lines (schematic only)
-x_past_low   <- 8
-x_past_high  <- 12
-x_fut_low    <- 8
-x_fut_high   <- 12
-x_body_low   <- 18
-x_body_high  <- 32
+tpcs<- as.data.frame(rbind( cbind(loc="temp", temp=temps, perf=temp.tpc), cbind(loc="trop", temp=temps, perf=trop.tpc) ))
+tpcs$temp= as.numeric(tpcs$temp)
+tpcs$perf= as.numeric(tpcs$perf)
 
-ggplot(tpc_schematic, aes(x = temp, y = perf, color = climate)) +
-  geom_smooth(se = FALSE, span = 0.5, size = 1.1) +
-  # Past mean horizontal line
-  geom_segment(
-    data = horiz,
-    aes(x = x_past_low, xend = x_past_high,
-        y = y_past, yend = y_past, color = climate),
-    inherit.aes = FALSE, linetype = "dashed"
-  ) +
-  # Future mean horizontal line
-  geom_segment(
-    data = horiz,
-    aes(x = x_fut_low, xend = x_fut_high,
-        y = y_future, yend = y_future, color = climate),
-    inherit.aes = FALSE, linetype = "solid"
-  ) +
-  # Body temperature range as a thick horizontal bar
-  geom_segment(
-    data = horiz,
-    aes(x = x_body_low, xend = x_body_high,
-        y = y_body_low, yend = y_body_low, color = climate),
-    inherit.aes = FALSE, size = 3, alpha = 0.4
-  ) +
-  geom_segment(
-    data = horiz,
-    aes(x = x_body_low, xend = x_body_high,
-        y = y_body_high, yend = y_body_high, color = climate),
-    inherit.aes = FALSE, size = 3, alpha = 0.4
-  ) +
-  scale_color_manual(values = c("Temperate" = "steelblue",
-                                "Tropical"  = "firebrick")) +
-  scale_x_continuous("Temperature (°C)", limits = c(0, 40)) +
-  scale_y_continuous("Relative performance", limits = c(0, 1.05)) +
-  theme_classic(base_size = 14) +
-  theme(legend.position = "top")
+#plot
+fig1b= ggplot(tpcs, aes(x=temp, y= perf, color=loc)) + 
+  geom_line(linewidth=2)+
+  geom_point(
+    data = tann,
+    mapping = aes(x = tmean_mean, y = 0, pch=period), size=3 )+  #+ inherit.aes = FALSE
+  scale_color_manual(values=c("darkorange","cornflowerblue"))+
+  theme_classic()+
+  ylab("Performance")+
+  xlab("Body temperature (C)")
 
-
-
+#geom_segment
